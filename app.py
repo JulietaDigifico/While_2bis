adfed49879749856c3be45a4505adf081686751c
Julieta <julieta.digifico@gmail.com>
2024-02-22 22:24:34 -0300

    Ejercicio2bis While

---
+++ app.py
@@ -0,0 +1,44 @@
+import tkinter
+from tkinter.messagebox import showinfo as alert
+from tkinter.messagebox import askyesno as question
+from tkinter.simpledialog import askstring as prompt
+import customtkinter
+
+
+'''
+nombre: Julieta
+apellido: Digifico
+---
+Ejercicio: while_02bis
+---
+Enunciado:
+Al presionar el botón ‘Mostrar Iteración’, mostrar mediante alert la suma 
+de los numeros pares comprendidos entre el 1 y el 10.
+'''
+
+
+class App(customtkinter.CTk):
+    
+    def __init__(self):
+        super().__init__()
+
+        self.title("UTN FRA")
+        
+        self.btn_mostrar_iteracion = customtkinter.CTkButton(master=self, text="Mostrar iteración", command=self.btn_mostrar_iteracion_on_click)
+        self.btn_mostrar_iteracion.grid(row=2, pady=20, columnspan=2, sticky="nsew")
+        
+    
+    def btn_mostrar_iteracion_on_click(self):
+        suma_pares = 0
+        numero = 1
+        while numero <= 10:
+            if numero % 2 == 0:
+                suma_pares += numero
+                numero += 1
+                alert("Mensaje", str(suma_pares))
+
+        
+if __name__ == "__main__":
+    app = App()
+    app.geometry("300x300")
+    app.mainloop()
